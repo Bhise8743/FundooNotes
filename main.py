03f6de15c24cfee12adee5707a9032331c98835c
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2024-02-08 15:05:41 +0530

    [Omkar] Add : added the middleware

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, Security, Depends, Request
 from fastapi.security import APIKeyHeader
-from Core.utils import JWT
+from Core.utils import JWT, request_logger
 from routes.user import user_router
 from routes.notes import notes_router
 from routes.labels import label_router
+
 app = FastAPI(title="Fundoo Notes")
+
+
+@app.middleware('/http')
+async def add_middleware(request: Request, call_next):
+    response = await call_next(request)
+    request_logger(request)
+    return response
+
 
 app.include_router(user_router, prefix='/user')
 app.include_router(notes_router, prefix='/notes',
