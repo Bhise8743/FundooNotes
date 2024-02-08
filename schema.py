8d1de74324a4d18f81a1194207862a71f3e016b6
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2024-02-08 11:30:27 +0530

    [Omkar] Add : added the schema of the labels

--- schema.py
+++ schema.py
@@ -11,14 +11,19 @@ class UserLogin(BaseModel):
 class UserDetails(UserLogin):
     first_name: str = Field('Enter first name', pattern=r'^[A-Z]{1}\D{1,}$')
     last_name: str = Field('Enter last name', pattern=r'^[A-Z]{1}\D{1,}$')
-    email: EmailStr  = Field('Enter email',)# Field(pattern=r'^[A-Za-z0-9]\w{8,}')
+    email: EmailStr = Field('Enter email', )  # Field(pattern=r'^[A-Za-z0-9]\w{8,}')
     # zero or more A-Z,a-z,0-9,any char all digits combination min 8 | not end with spaces
-    phone: int =Field('Enter phone number') # = Field(pattern=r'^[6-9]\d{9}$')
+    phone: int = Field('Enter phone number')  # = Field(pattern=r'^[6-9]\d{9}$')
     city: str = Field('Enter city')
     state: str = Field('Enter state ')
+
 
 class UserNotes(BaseModel):
     title: str = Field("Enter title ", pattern=r'^[A-Z]{1}\D{1,}')
     description: str
     color: str
-    user_id:int
+    user_id: int
+
+
+class LabelSchema(BaseModel):
+    label_name: str = Field('Field name')
