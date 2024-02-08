13f2224f29877dc657fccb14f842e7af1cec72c2
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2024-02-08 18:01:40 +0530

    [Omkar] Add : added the user test apis

---
+++ Test/test_user_apis.py
@@ -0,0 +1,12 @@
+def test_user_registration(client, user_data):
+    response = client.post('/user/register', json=user_data)
+    assert response.status_code == 201
+
+
+def test_login_user(client, user_data, login_data):
+    response = client.post('/user/register', json=user_data)
+    assert response.status_code == 201
+
+    response = client.post('/user/login', json=login_data)
+    assert response.status_code == 200
+
