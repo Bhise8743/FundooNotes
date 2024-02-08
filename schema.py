6bf14525dd0eeae86bd86176cd4821e1ea943570
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2024-02-08 18:02:50 +0530

    [Omkar] Add : added the is_verifed as optional for testing

--- schema.py
+++ schema.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, Field, EmailStr, SecretStr
-from typing import List
+from pydantic import BaseModel, Field, EmailStr
+from typing import List, Optional
 
 
 # globally validate the mobile numbers
@@ -12,10 +12,10 @@ class UserDetails(UserLogin):
     first_name: str = Field('Enter first name', pattern=r'^[A-Z]{1}\D{1,}$')
     last_name: str = Field('Enter last name', pattern=r'^[A-Z]{1}\D{1,}$')
     email: EmailStr = Field('Enter email')
-
     phone: int = Field('Enter phone number')
     city: str = Field('Enter city')
     state: str = Field('Enter state ')
+    is_verified: Optional[bool] = Field(default=False)
 
 
 class UserNotes(BaseModel):
