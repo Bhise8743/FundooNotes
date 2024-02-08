298350c3d2f827f19c81a94a47dd315bfae540ed
Bhise Omakr Balaji <omkarbhise8743@gmail.com>
2024-02-08 17:22:17 +0530

    [Omkar] Add :  the collaborator schema

--- schema.py
+++ schema.py
@@ -11,9 +11,9 @@ class UserLogin(BaseModel):
 class UserDetails(UserLogin):
     first_name: str = Field('Enter first name', pattern=r'^[A-Z]{1}\D{1,}$')
     last_name: str = Field('Enter last name', pattern=r'^[A-Z]{1}\D{1,}$')
-    email: EmailStr = Field('Enter email', )  # Field(pattern=r'^[A-Za-z0-9]\w{8,}')
-    # zero or more A-Z,a-z,0-9,any char all digits combination min 8 | not end with spaces
-    phone: int = Field('Enter phone number')  # = Field(pattern=r'^[6-9]\d{9}$')
+    email: EmailStr = Field('Enter email')
+
+    phone: int = Field('Enter phone number')
     city: str = Field('Enter city')
     state: str = Field('Enter state ')
 
@@ -22,8 +22,12 @@ class UserNotes(BaseModel):
     title: str = Field("Enter title ", pattern=r'^[A-Z]{1}\D{1,}')
     description: str
     color: str
-    user_id: int
 
 
 class LabelSchema(BaseModel):
     label_name: str = Field('Field name')
+
+
+class CollaboratorDetails(BaseModel):
+    user_ids: List[int]
+    note_id: int
